d7b73b6ec0a923559603e9a5cda468abb3378ff3
Craig Meinschein <cmeinsch@gmail.com>
2012-04-02 11:13:17 -0700

    Added Google RSS ticker.

--- plugins/plugin_google.rb
+++ plugins/plugin_google.rb
@@ -12,35 +12,72 @@
 class GoogleRSS
 	include Cinch::Plugin
 	
+	@@active = false
+	@@current_msg = String.new
+
 	require 'sanitize'
 	require 'rss/1.0'
 	require 'rss/2.0'
 
 	match /rss on$/i, method: :start_ticker
 	match /rss off$/i, method: :stop_ticker
-	match /rss$/, method: :report_status
+	match /rss$/i, method: :report_status
 
 	def start_ticker(m)
-		source = "http://www.google.com/appsstatus/rss/en"
-		raw = String.new
-		open(source) do |input|
-			raw = input.read
-		end
-		rss = RSS::Parser.parse(raw, false)
+		m.reply "Google RSS: On"
+		if (!@@active)
+			@@active = true
 
-		max_msg_size = 512 - m.bot.nick.size - m.channel.name.size - 43
+			while (@@active)
+				source = "http://www.google.com/appsstatus/rss/en"
+				raw = String.new
+				open(source) do |input|
+					raw = input.read
+				end
+				rss = RSS::Parser.parse(raw, false)
+
+				if (rss.items.size > 0)
+					if (rss.items[0].description.hash != @@current_msg.hash)
+						@@current_msg = rss.items[0].description
+						max_msg_size = 512 - m.bot.nick.size - m.channel.name.size - 43
+	
+						cleaned_rss = Sanitize.clean(rss.items[0].description)
+						msg_set = cleaned_rss.split "\u00A0"
 		
-		cleaned_rss = Sanitize.clean(rss.items[0].description)
-		msg_set = cleaned_rss.split "\u00A0"
+						reply = "[#{rss.items[0].title}] "
+						reply << "#{msg_set[0]}"
+						if (reply.size < max_msg_size)
+							reply << "#{msg_set[1]}"
+						end
+						m.reply reply[0,max_msg_size]
+						m.reply "More info at: #{rss.items[0].link}"
+					end
+				end
 
-		reply = "[#{rss.items[0].title}] "
-		reply << "#{msg_set[1][0,max_msg_size]}"
-		m.reply reply
-		m.reply "More info at: #{rss.items[0].link}"
+				# This is kind of janky but I'm not sure how else to do it.
+				for i in 0..15
+					sleep(1)
+					if (!@@active)
+						return
+					end
+				end
+
+			end
+		end
 	end
 
-	def execute(m)
-		m.reply "This is a test."
+	def stop_ticker(m)
+		@@active = false
+		@@current_msg = ""
+		m.reply "Google RSS: Off"
+	end
+
+	def report_status(m)
+		if (@@active)
+			m.reply "Google RSS: On"
+		else
+			m.reply "Google RSS: Off"
+		end
 	end
 
 	def google_help(m)
