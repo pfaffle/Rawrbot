abdd70bc2a9950c9312f956151b3324df65e8ef8
Craig Meinschein <cmeinsch@gmail.com>
2012-07-10 14:40:44 -0700

    RSS ticker starts on connect, reports to channels listed in config.

--- plugins/plugin_google.rb
+++ plugins/plugin_google.rb
@@ -12,7 +12,7 @@
 class GoogleRSS
 	include Cinch::Plugin
 	
-	@@active = false
+	@@active = true
 	@@current_msg = String.new
 
 	require 'sanitize'
@@ -23,56 +23,20 @@ class GoogleRSS
 	match /rss off$/i, method: :stop_ticker
 	match /rss$/i, method: :report_status
 
+	listen_to :connect, method: :run_ticker
+
 	def start_ticker(m)
 		m.reply "Google RSS: On"
 		if (!@@active)
 			@@active = true
-
-			while (@@active)
-				source = "http://www.google.com/appsstatus/rss/en"
-				raw = String.new
-				open(source) do |input|
-					raw = input.read
-				end
-				rss = RSS::Parser.parse(raw, false)
-
-				if (rss.items.size > 0)
-					if (rss.items[0].description.hash != @@current_msg.hash)
-						@@current_msg = rss.items[0].description
-						max_msg_size = 512 - m.bot.nick.size - m.channel.name.size - 43
-	
-						cleaned_rss = Sanitize.clean(rss.items[0].description)
-						msg_set = cleaned_rss.split "\u00A0"
-		
-						reply = "[#{rss.items[0].title}] "
-						reply << "#{msg_set[0]}"
-						if (reply.size < max_msg_size)
-							reply << "#{msg_set[1]}"
-						end
-						m.reply reply[0,max_msg_size]
-						m.reply "More info at: #{rss.items[0].link}"
-					end
-				end
-
-				# This is kind of janky but I'm not sure how else to do it.
-				for i in 0..15
-					sleep(1)
-					if (!@@active)
-						return
-					end
-					if (m.bot.quitting)
-						return
-					end
-				end
-
-			end
+			run_ticker(m)
 		end
 	end
 
 	def stop_ticker(m)
+		m.reply "Google RSS: Off"
 		@@active = false
 		@@current_msg = ""
-		m.reply "Google RSS: Off"
 	end
 
 	def report_status(m)
@@ -80,6 +44,51 @@ class GoogleRSS
 			m.reply "Google RSS: On"
 		else
 			m.reply "Google RSS: Off"
+		end
+	end
+
+	def run_ticker(m)
+		load "#{$pwd}/plugins/config_google.rb"
+		source = "http://www.google.com/appsstatus/rss/en"
+		channel_list = google_return_config
+		
+		while (@@active)
+			raw = String.new
+			open(source) do |input|
+				raw = input.read
+			end
+			rss = RSS::Parser.parse(raw, false)
+
+			if (rss.items.size > 0)
+				if (rss.items[0].description.hash != @@current_msg.hash)
+					@@current_msg = rss.items[0].description
+
+					cleaned_rss = Sanitize.clean(rss.items[0].description)
+					msg_set = cleaned_rss.split "\u00A0"
+	
+					reply = "[#{rss.items[0].title}] "
+					reply << "#{msg_set[0]}"
+					
+					# Report RSS results to each channel in the list.
+					channel_list.each do |chname|
+						max_msg_size = 512 - m.bot.nick.size - chname.size - 43
+						Channel(chname).send reply[0,max_msg_size]
+						Channel(chname).send "More info at: #{rss.items[0].link}"
+					end
+				end
+			end
+
+			# This is kind of janky but I'm not sure how else to do it.
+			for i in 0..15
+				sleep(1)
+				if (!@@active)
+					return
+				end
+				if (m.bot.quitting)
+					return
+				end
+			end
+
 		end
 	end
 
@@ -95,3 +104,5 @@ class GoogleRSS
 		m.reply "See: !help google"
 	end
 end
+# End of plugin: Google RSS Feed
+# =============================================================================
