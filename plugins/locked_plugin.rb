fdcdc79ac0546ec112a4c578ba27abc4a095f96f
Craig Meinschein <cmeinsch@gmail.com>
2013-09-24 12:26:06 -0700

    Fixed lockedouttime plugin for new LDAP code.

--- plugins/locked_plugin.rb
+++ plugins/locked_plugin.rb
@@ -1,24 +1,45 @@
 class LockOutTime
-  include Cinch::Plugin
-
-  require File.expand_path('../../lib/util.rb', __FILE__)
-
-  match(/!locked (\w+)/, :use_prefix => false)
-  match(/!lockoutTime (\w+)/, :use_prefix => false)
-
-  def execute(m, query)
+    include Cinch::Plugin
+  
+    require "#{$pwd}/lib/ldap_helper.rb"
+    require "#{$pwd}/lib/util.rb"
+  
+    match(/locked (\w+)$/)
+    match(/lockoutTime (\w+)$/)
+  
+    def execute(m, query)
     ldap = LdapHelper.new('cecs')
     util = Util.new
-    attributes = ['lockoutTime']
-    output = ldap.search(query, attributes, 'sAMAccountName')
-    output.flatten!
-    output.each { |time|
-      if time.to_s == "0"
-        m.reply("Account is not locked")
-      else
-        m.reply("Account has been locked since #{util.dos2unixtime(time)}")
-      end
-    }
+
+    # Error-checking to sanitize input. i.e. no illegal symbols.
+    if (query =~ /[^\w@._-]/)
+        m.reply("Invalid search query '#{query}'")
+        return
+    end
+    reply = String.new()
+
+    query.downcase!
+    result = ldap.search('sAMAccountName',query)
+
+    # Check for errors.
+    if (!result)
+        m.reply "Error: LDAP query failed. Check configuration."
+    else
+        if (result['dn'].empty?)
+            reply = "Error: No results.\n"
+        elsif (result['dn'].length > 1)
+            reply = "Error: Too many results.\n"
+        else
+            result['lockouttime'].each do |time|
+                if time.to_s == "0"
+                    m.reply("Account is not locked")
+                else
+                    m.reply("Account has been locked since #{util.dos2unixtime(time)}")
+                end
+            end
+        end
+        m.reply(reply)
+    end
   end
 
 end
