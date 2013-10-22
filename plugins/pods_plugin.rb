35e3933561f4d624e554f87d99a2e1b68311698a
Craig Meinschein <cmeinsch@gmail.com>
2013-10-22 14:40:32 -0700

    Fixed pods plugin to use ldap_helper

--- plugins/pods_plugin.rb
+++ plugins/pods_plugin.rb
@@ -1,15 +1,40 @@
 class Pods
   include Cinch::Plugin
 
-  match(/!pods (\w+)/, :use_prefix => false)
+  require "#{$pwd}/lib/ldap_helper.rb"
+
+  match(/pods (\w+)$/)
 
   def execute(m, query)
     ldap = LdapHelper.new('cat')
-    util = Util.new
-    attributes = ['pod']
-    pods = ldap.search(query, attributes, 'uid')
-    pods.flatten!
-    m.reply(pods.join(" "))
+
+    # Error-checking to sanitize input. i.e. no illegal symbols.
+    if (query =~ /[^\w@._-]/)
+        m.reply("Invalid search query '#{query}'")
+        return
+    end
+    reply = String.new()
+
+    query.downcase!
+    result = ldap.search('uid',query)
+
+    # Check for errors.
+    if (!result)
+        m.reply "Error: LDAP query failed. Check configuration."
+    else
+        if (result['dn'].empty?)
+            reply = "Error: No results.\n"
+        elsif (result['dn'].length > 1)
+            reply = "Error: Too many results.\n"
+        else
+            reply = "pods:"
+            result['pod'].each do |pod|
+                reply += " #{pod}"
+            end
+            reply += "\n"
+        end
+        m.reply(reply)
+    end
   end
 
 end
