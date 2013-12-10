ef4876e9a1540bf51cbcbfb361b4a6c46c5bcd9d
Craig Meinschein <cmeinsch@gmail.com>
2013-12-09 16:46:37 -0800

    Fixed pods plugin to use new LDAP code.

--- plugins/pods_plugin.rb
+++ plugins/pods_plugin.rb
@@ -1,39 +1,38 @@
 class Pods
-  include Cinch::Plugin
+    include Cinch::Plugin
 
-  require "#{$pwd}/lib/ldap_helper.rb"
+    require "#{$pwd}/lib/ldap_helper.rb"
 
-  match(/pods (\w+)$/)
+    match(/pods (\w+)$/)
 
-  def execute(m, query)
-    ldap = LdapHelper.new('cat')
+    def execute(m, query)
+        ldap = LdapHelper.new('cat')
 
-    # Error-checking to sanitize input. i.e. no illegal symbols.
-    if (query =~ /[^\w@._-]/)
-        m.reply("Invalid search query '#{query}'")
-        return
-    end
-    reply = String.new()
-
-    query.downcase!
-    result = ldap.search('uid',query)
-
-    # Check for errors.
-    if (!result)
-        m.reply "Error: LDAP query failed. Check configuration."
-    else
-        if (result['dn'].empty?)
-            reply = "Error: No results.\n"
-        elsif (result['dn'].length > 1)
-            reply = "Error: Too many results.\n"
-        else
-            result['pod'].each do |pod|
-                reply += "#{pod} "
-            end
-            reply += "\n"
+        # Error-checking to sanitize input. i.e. no illegal symbols.
+        if (query =~ /[^\w@._-]/)
+            m.reply("Invalid search query '#{query}'")
+            return
         end
+        reply = String.new()
+
+        query.downcase!
+        result = ldap.search('uid',query)
+
+        # Check for errors.
+        if (!result)
+            m.reply "Error: LDAP query failed. Check configuration.\n"
+            return
+        elsif (result.empty?)
+            User(m.user.nick).send("Error: No results.\n")
+            return
+        end
+
+        catEntry = result[0]
+        catEntry[:pod].each do |pod|
+            reply += "#{pod} "
+        end
+        reply += "\n"
+
         m.reply(reply)
     end
-  end
-
 end
