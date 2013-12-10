81830cdd588619cdfd4ffcb64ceb76736924e08f
Craig Meinschein <cmeinsch@gmail.com>
2013-12-09 16:42:34 -0800

    Fixed lockedout plugin to use new LDAP code.

--- plugins/locked_plugin.rb
+++ plugins/locked_plugin.rb
@@ -8,38 +8,37 @@ class LockOutTime
     match(/lockoutTime (\w+)$/)
   
     def execute(m, query)
-    ldap = LdapHelper.new('cecs')
-    util = Util.new
+	    ldap = LdapHelper.new('cecs')
+	    util = Util.new
+	
+	    # Error-checking to sanitize input. i.e. no illegal symbols.
+	    if (query =~ /[^\w@._-]/)
+	        m.reply("Invalid search query '#{query}'")
+	        return
+	    end
+	    reply = String.new()
+	
+	    query.downcase!
+	    result = ldap.search('sAMAccountName',query)
+	
+	    # Check for errors.
+	    if (!result)
+	        m.reply "Error: LDAP query failed. Check configuration.\n"
+	        return
+	    elsif (result.empty?)
+	        User(m.user.nick).send("Error: No results.\n")
+	        return
+	    end
+	
+	    catEntry = result[0]
+	    time = catEntry[:lockouttime][0]
+	
+	    if time.to_s == "0"
+	        reply = "Account is not locked."
+	    else
+	        reply = "Account has been locked since #{util.dos2unixtime(time)}."
+	    end
 
-    # Error-checking to sanitize input. i.e. no illegal symbols.
-    if (query =~ /[^\w@._-]/)
-        m.reply("Invalid search query '#{query}'")
-        return
+	    m.reply(reply)
     end
-    reply = String.new()
-
-    query.downcase!
-    result = ldap.search('sAMAccountName',query)
-
-    # Check for errors.
-    if (!result)
-        m.reply "Error: LDAP query failed. Check configuration."
-    else
-        if (result['dn'].empty?)
-            reply = "Error: No results.\n"
-        elsif (result['dn'].length > 1)
-            reply = "Error: Too many results.\n"
-        else
-            result['lockouttime'].each do |time|
-                if time.to_s == "0"
-                    m.reply("Account is not locked")
-                else
-                    m.reply("Account has been locked since #{util.dos2unixtime(time)}")
-                end
-            end
-        end
-        m.reply(reply)
-    end
-  end
-
 end
