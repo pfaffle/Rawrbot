0c8b4db022dd15ea23cb834b3efc7339be09732d
Craig Meinschein <cmeinsch@gmail.com>
2014-07-03 19:49:38 -0700

    Wrote new import_karma script to use sqlite database.

--- tools/import_karma.rb
+++ tools/import_karma.rb
@@ -6,11 +6,11 @@
 # Import karma information from a flat file into a sqlite3 database. This script
 # will replace any duplicate key information in the database with information
 # from the flat file. In other words, it assumes the flat file is authoritative.
-# Takes two command-line arguments.
+# Takes one command-line argument.
 #
-# The first argument is the path to the flat file which contains the karma
-# information to import into the database. The file should be formatted as a
-# series of key-value pairs, separated by a delimiter. Only one key-value pair
+# The command-line argument is the path to the flat file which contains the
+# karma information to import into the database. The file should be formatted as
+# a series of key-value pairs, separated by a delimiter. Only one key-value pair
 # should exist per line. For example:
 #
 # foo => 30
@@ -19,15 +19,11 @@
 # The delimiter can be edited here:
 #
 delimiter = ' => '
-#
-# The second argument is the path to the sqlite database file to which you would
-# like to import karma information. Be sure the bot is not running and using
-# that database when you begin importing data.
 
 require 'sqlite3'
 
 source = ARGV[0]
-target = ARGV[1]
+target = 'karma.sqlite3'
 
 # Check if input file exists.
 if !(File.exists?(source))
@@ -45,16 +41,18 @@ puts("Importing...")
 File.open(source, 'r:UTF-8') do |file|
   while (line = file.gets)
     begin
-      line.delete!("\r","\n")
+      line.delete!("\r")
+      line.delete!("\n")
       line =~ /(.+)#{delimiter}(.+)/i 
     rescue => e
       warn("Failed to parse line. Error: #{e}\nLine: #{line}")
-      break
+      next
     end
     key = $1
     val = $2
     r = db.get_first_value("SELECT val FROM karma WHERE obj=?", key)
     begin
+      next if (val == '0')
       if (r.nil?)
         # Element does not yet exist in the db; insert it.
         db.execute("INSERT INTO karma (obj,val) VALUES (?,?)", key, val)
@@ -64,7 +62,7 @@ File.open(source, 'r:UTF-8') do |file|
       end
     rescue => e
       warn("Failed to insert data. Error: #{e}\nLine: #{line}")
-      break
+      next
     end
   end
 end
