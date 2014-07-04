340d60a02aac9f4947a9526df70e38a0d736d9e8
Craig Meinschein <cmeinsch@gmail.com>
2014-07-03 20:22:08 -0700

    Wrote new import_learning script to use sqlite database.

--- tools/import_learning.rb
+++ tools/import_learning.rb
@@ -1,59 +1,70 @@
 #!/usr/bin/env ruby
+# ==============================================================================
 # Import Learning
+# ==============================================================================
 #
-# Import learned factoids from a flat file into a GDBM database.
-# This script will non-destructively append data from the flat
-# file to the database, if the key already exists.
-# Takes two command-line arguments.
+# Import learned factoids from a flat file into a sqlite database. This script
+# will non-destructively append data from the flat file to the database, if the
+# key already exists. Takes one command-line argument.
 #
-# The first argument is the path to a flat file which contains 
-# the information to import into the database. The file
-# should be formatted as a series of key-value pairs, separated
-# by a delimiter. Only one key-value pair should exist per line.
-# For example:
+# The command-line argument is the path to the flat file which contains the
+# information to import into the database. The file should be formatted as a
+# series of key-value pairs, separated by a delimiter. Only one key-value pair
+# should exist per line. For example:
 #
 # foo => a silly person
 # bar => a solid object or a difficult test or a place to beer
 #
-#    The delimiter can be edited here:
+# The delimiter can be edited here:
 #
 delimiter = ' => '
-#
-# The second argument is the path to the GDBM database file to which
-# you would like to import information. Be sure rawrbot is not
-# running and using that database when you begin importing data.
 
-source_text = ARGV[0]
-target_db = ARGV[1]
+require 'sqlite3'
 
-require 'gdbm'
+source = ARGV[0]
+target = 'learning.sqlite3'
 
-# Check if the input and output files exist. Prompt user to
-# create output file if it is missing.
-if !(File.exists? source_text)
-    abort "File #{source_text} not found. Aborting.\n"
-elsif !(File.exists? target_db)
-    print "File #{target_db} not found. Create new database? "
-    create_db = STDIN.gets
-    if create_db =~ /ye?s?/i
-        # continue
-    elsif create_db =~ /no?/i
-        abort "Aborting.\n"
-    else
-        abort "Unrecognized input. Aborting.\n"
-    end
+# Check if input file exists.
+if !(File.exists?(source))
+  abort("Source file #{source} not found. Aborting.\n")
 end
+
+# Initialize database.
+db = SQLite3::Database.new(target)
+db.execute("CREATE TABLE IF NOT EXISTS learning(
+              thing TEXT PRIMARY KEY,
+              info TEXT)")
 
 # Import data into database.
-File.open(source_text, 'r') do |file|
-    GDBM.open(target_db) do |db|
-        while (line = file.gets)
-            line =~ /(.+)#{delimiter}(.+)/i 
-            if db.has_key? "#{$1}"
-                db["#{$1}"] = "#{db["#{$1}"]} or #{$2}"
-            else
-                db["#{$1}"] = "#{$2}"
-            end
-        end
+puts("Importing...")
+File.open(source, 'r:UTF-8') do |file|
+  while (line = file.gets)
+    begin
+      line.delete!("\r")
+      line.delete!("\n")
+      line =~ /(.+)#{delimiter}(.+)/i 
+    rescue => e
+      warn("Failed to parse line. Error: #{e}\nLine: #{line}")
+      next
     end
+    key = $1
+    val = $2
+    r = db.get_first_value("SELECT info FROM learning WHERE thing=?", key)
+    begin
+      next if (val == '')
+      if (r.nil?)
+        # Element does not yet exist in the db; insert it.
+        db.execute("INSERT INTO learning (thing,info) VALUES (?,?)", key, val)
+      else
+        # Element already exists in the db; update it.
+        db.execute("UPDATE learning SET info=? WHERE thing=?",
+                   "#{r} or #{val}",
+                   key)
+      end
+    rescue => e
+      warn("Failed to insert data. Error: #{e}\nLine: #{line}")
+      next
+    end
+  end
 end
+puts("Done.")
