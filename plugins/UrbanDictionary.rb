4c624cc523a6af4d494a07fd7780f3df93544a64
Craig Meinschein <cmeinsch@gmail.com>
2015-03-14 23:47:36 -0700

    Fixed prefix for UrbanDictionary plugin.

--- plugins/UrbanDictionary.rb
+++ plugins/UrbanDictionary.rb
@@ -4,8 +4,10 @@ require 'nokogiri'
 class UrbanDictionary
   include Cinch::Plugin
 
-  set :prefix, /^!/
+  set :prefix, lambda { |m| m.bot.config.plugins.prefix }
+
   match /urban (.+)/
+
   def lookup(word)
     url = "http://www.urbandictionary.com/define.php?term=#{CGI.escape(word)}"
     CGI.unescape_html Nokogiri::HTML(open(url)).at("div.definition").text.gsub(/\s+/, ' ') rescue nil
