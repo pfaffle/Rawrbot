95c961f03b8aba3e12c85675590dcf163cbbce01
Craig Meinschein <cmeinsch@gmail.com>
2015-07-17 20:42:51 -0700

    Added Reactions plugin to respond to keywords.

---
+++ plugins/Reactions.rb
@@ -0,0 +1,20 @@
+class Reactions
+  include Cinch::Plugin
+
+  set :prefix, lambda { |m| m.bot.config.plugins.prefix }
+
+  match //, :use_prefix => false
+
+  def initialize(*args)
+    super
+    @reactions = YAML.load(File.read("config/reactions.yml"))
+  end
+
+  def execute(m)
+    @reactions.each do |trigger, response|
+      if m.message =~ /#{trigger}/i
+        m.reply(response)
+      end
+    end
+  end
+end
