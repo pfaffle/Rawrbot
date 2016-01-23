b01d834a37a9bbef5ac0f4e9e6d5c59183b68040
Craig Meinschein <cmeinsch@gmail.com>
2016-01-23 12:30:52 -0800

    Add tests for neutral karma

--- spec/Karma_spec.rb
+++ spec/Karma_spec.rb
@@ -1,5 +1,10 @@
 require 'plugins/Karma'
 
+def delete_key_from_db(db, key)
+  db.execute('DELETE FROM karma WHERE key=?', key)
+  expect(db.get_first_value('SELECT val FROM karma WHERE key=?', key)).to eq nil
+end
+
 RSpec.describe 'Karma#display' do
   before(:each) do
     @bot = make_bot()
@@ -8,20 +13,21 @@ RSpec.describe 'Karma#display' do
   end
 
   context 'key does not exist in the database' do
+    let(:key) { 'imatestvalue' }
+    let(:db) { 'karma.sqlite3' }
+
+    before(:each) do
+      # TODO: refactor Karma plugin so we can use a test-only db here
+      delete_key_from_db(SQLite3::Database.new(db), key)
+    end
+
     it 'replies once' do
-      msg = make_message(@bot, '!karma foo'
-      replies = get_replies(msg)
-      expect(replies.length).to eq 1
+      msg = make_message(@bot, "!karma #{key}", channel: '#testchan')
+      expect(get_replies(msg).length).to eq 1
     end
     it 'shows karma as neutral' do
-      msg = make_message(@bot, '!karma foo')
-      replies = get_replies(msg)
-      expect(replies[0].text).to eq 'foo has neutral karma.'
-    end
-    it 'derps' do
-      msg = make_message(@bot, '!karma foo')
-      replies = get_replies(msg)
-      expect(replies).to eq nil
+      msg = make_message(@bot, "!karma #{key}", channel: '#testchan')
+      expect(get_replies(msg)[0].text).to eq "#{key} has neutral karma."
     end
   end
 end
