fe7fa834b4cce7b6a7c82af5a9a89db8672ee575
Craig Meinschein <cmeinsch@gmail.com>
2016-01-23 18:45:28 -0800

    Add Karma tests for multi-word keys

--- spec/Karma_spec.rb
+++ spec/Karma_spec.rb
@@ -11,17 +11,6 @@ def set_db_key_value(db, key, val)
   expect(db.get_first_value('SELECT val FROM karma WHERE key=?', key)).to eq val
 end
 
-def get_random_number_list()
-  list = []
-  max_num = 1000
-  rng = Random.new()
-  3.times do
-    list << rng.rand(max_num)
-    list << -rng.rand(max_num)
-  end
-  return list.shuffle
-end
-
 RSpec.describe 'Karma#display' do
   before(:each) do
     @bot = make_bot()
@@ -29,44 +18,46 @@ RSpec.describe 'Karma#display' do
     @bot.plugins.register_plugin(Karma)
   end
 
-  context 'key does not exist in the database' do
-    let(:key) { 'imatestvalue' }
-    let(:db) { 'karma.sqlite3' }
+  ['imatestkey', 'multi word key'].each do |key|
+    let(:karma_key) { key }
 
-    before(:each) do
-      # TODO: refactor Karma plugin so we can use a test-only db here
-      delete_key_from_db(SQLite3::Database.new(db), key)
-    end
-
-    it 'replies once' do
-      msg = make_message(@bot, "!karma #{key}", channel: '#testchan')
-      expect(get_replies(msg).length).to eq 1
-    end
-    it 'shows karma as neutral' do
-      msg = make_message(@bot, "!karma #{key}", channel: '#testchan')
-      expect(get_replies(msg)[0].text).to eq "#{key} has neutral karma."
-    end
-  end
-
-  context 'key has a value in the database' do
-    let(:key) { 'imatestvalue' }
-    let(:db) { 'karma.sqlite3' }
-
-    get_random_number_list().each do |val|
-      let(:karma_value) { val }
+    context 'key does not exist in the database' do
+      let(:db) { 'karma.sqlite3' }
 
       before(:each) do
         # TODO: refactor Karma plugin so we can use a test-only db here
-        set_db_key_value(SQLite3::Database.new(db), key, karma_value)
+        delete_key_from_db(SQLite3::Database.new(db), karma_key)
       end
 
       it 'replies once' do
-        msg = make_message(@bot, "!karma #{key}", channel: '#testchan')
+        msg = make_message(@bot, "!karma #{karma_key}", channel: '#testchan')
         expect(get_replies(msg).length).to eq 1
       end
-      it 'shows existing karma value' do
-        msg = make_message(@bot, "!karma #{key}", channel: '#testchan')
-        expect(get_replies(msg)[0].text).to eq "#{key} has karma of #{karma_value}."
+      it 'shows karma as neutral' do
+        msg = make_message(@bot, "!karma #{karma_key}", channel: '#testchan')
+        expect(get_replies(msg)[0].text).to eq "#{karma_key} has neutral karma."
+      end
+    end
+
+    context 'key has a value in the database' do
+      let(:db) { 'karma.sqlite3' }
+
+      [-9999, -1, 1, 10034].each do |val|
+        let(:karma_value) { val }
+
+        before(:each) do
+          # TODO: refactor Karma plugin so we can use a test-only db here
+          set_db_key_value(SQLite3::Database.new(db), karma_key, karma_value)
+        end
+
+        it 'replies once' do
+          msg = make_message(@bot, "!karma #{karma_key}", channel: '#testchan')
+          expect(get_replies(msg).length).to eq 1
+        end
+        it 'shows existing karma value' do
+          msg = make_message(@bot, "!karma #{karma_key}", channel: '#testchan')
+          expect(get_replies(msg)[0].text).to eq "#{karma_key} has karma of #{karma_value}."
+        end
       end
     end
   end
