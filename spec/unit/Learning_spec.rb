796b773b3dcbe5a1751c0a55aed7445aef0620c3
Craig Meinschein <cmeinsch@gmail.com>
2017-05-22 21:47:39 -0700

    Add more learning plugin tests
    Add tests around the $who and <reply> special keywords
    
    Still need tests around |, which might be a little trickier since they
    involve some randomness.

--- spec/unit/Learning_spec.rb
+++ spec/unit/Learning_spec.rb
@@ -1,46 +1,40 @@
 require 'plugins/Learning'
 require 'database_helper'
 
+# NOTE: When using cinch-test, the nick of the user sending the message is
+# always 'test' and can't be overridden.
+
 def get_replies_to(message)
   get_replies(make_message(@bot, message, channel: channel))
 end
 
 RSpec::Matchers.define :be_an_acknowledgement do
-  # When using cinch-test, the nick of the user sending the message is
-  # always 'test' and can't be overridden.
-  usr = 'test'
   match do |actual|
-    ["good to know, #{usr}.", "got it, #{usr}.", "roger, #{usr}.",
-     "understood, #{usr}.", "OK, #{usr}.", "so speaketh #{usr}.",
-     "whatever you say, #{usr}.", "I'll take your word for it, #{usr}."]
-        .include?(actual.text)
+    ['good to know, test.', 'got it, test.', 'roger, test.',
+     'understood, test.', 'OK, test.', 'so speaketh test.',
+     'whatever you say, test.', "I'll take your word for it, test."]
+      .include?(actual.text)
   end
 end
 
 RSpec::Matchers.define :be_a_successful_edit do
-  # When using cinch-test, the nick of the user sending the message is
-  # always 'test' and can't be overridden.
-  usr = 'test'
   match do |actual|
-    "done, #{usr}." == actual.text
+    'done, test.' == actual.text
   end
 end
 
 RSpec::Matchers.define :give_up do
-  # When using cinch-test, the nick of the user sending the message is
-  # always 'test' and can't be overridden.
-  usr = 'test'
   match do |actual|
-    ["bugger all, I dunno, #{usr}.", "no idea, #{usr}.", "huh?", "what?",
-     "dunno, #{usr}."]
-        .include?(actual.text)
+    ['bugger all, I dunno, test.', 'no idea, test.', 'huh?', 'what?',
+     'dunno, test.']
+      .include?(actual.text)
   end
 end
 
 describe 'Learning' do
-  let(:db_file) { 'test_learning.sqlite3' }
-  let(:table) { 'learning' }
-  let(:bot_nick) { 'testbot' }
+  let(:db_file) {'test_learning.sqlite3'}
+  let(:table) {'learning'}
+  let(:bot_nick) {'testbot'}
 
   before(:each) do
     @bot = new_bot_with_plugins(Learning)
@@ -56,10 +50,10 @@ describe 'Learning' do
     File.delete(db_file)
   end
 
-  context 'bot does not know of item' do
-    let(:key) { 'foo' }
-    let(:value) { 'bar' }
-    let(:channel) { '#testchan' }
+  context 'bot does not know of entry' do
+    let(:key) {'foo'}
+    let(:value) {'bar'}
+    let(:channel) {'#testchan'}
 
     before(:each) do
       delete_key_from_db(@db, key)
@@ -99,10 +93,10 @@ describe 'Learning' do
     end
   end
 
-  context 'bot knows of item' do
-    let(:key) { 'foo' }
-    let(:value) { 'bar' }
-    let(:channel) { '#testchan' }
+  context 'bot knows of an entry without special keywords' do
+    let(:key) {'foo'}
+    let(:value) {'bar'}
+    let(:channel) {'#testchan'}
 
     before(:each) do
       set_db_key_value(@db, key, value)
@@ -148,5 +142,109 @@ describe 'Learning' do
     end
   end
 
-  # TODO: add some tests around |, <who> and <reply> special cases
+  context 'bot knows of an entry with the <reply> keyword' do
+    let(:key) {'foo'}
+    let(:value) {'<reply>bar'}
+    let(:channel) {'#testchan'}
+
+    before(:each) do
+      set_db_key_value(@db, key, value)
+    end
+
+    it 'should teach it' do
+      replies = get_replies_to("#{bot_nick}: #{key}")
+      expect(replies.length).to eq 1
+      expect(replies.first.text).to eq(value.gsub('<reply>', ''))
+    end
+
+    it 'should literally teach it' do
+      replies = get_replies_to("#{bot_nick}: literal #{key}")
+      expect(replies.length).to eq 1
+      expect(replies.first.text).to eq("#{key} =is= #{value}.")
+    end
+
+    it 'should be able to forget it' do
+      replies = get_replies_to("#{bot_nick}: forget #{key}")
+      expect(replies.length).to eq 1
+      expect(replies.first.text).to eq("I forgot #{key}.")
+      replies = get_replies_to("#{bot_nick}: #{key}")
+      expect(replies.length).to eq 1
+      expect(replies.first).to give_up
+    end
+
+    it 'should be able to edit it' do
+      replies = get_replies_to(
+        "#{bot_nick}: #{key} =~ s/#{Regexp.escape(value)}/baz/")
+      expect(replies.length).to eq 1
+      expect(replies.first).to be_a_successful_edit
+      replies = get_replies_to("#{bot_nick}: #{key}")
+      expect(replies.length).to eq 1
+      expect(replies.first.text).to eq("#{key} is baz.")
+    end
+
+    it 'should be able to add to it' do
+      replies = get_replies_to("#{bot_nick}: #{key} is baz")
+      expect(replies.length).to eq 1
+      expect(replies.first).to be_an_acknowledgement
+      replies = get_replies_to("#{bot_nick}: #{key}")
+      expect(replies.length).to eq 1
+      expect(replies.first.text)
+        .to eq("#{value.gsub('<reply>', '')} or baz")
+    end
+  end
+
+  context 'bot knows of an entry with the $who keyword' do
+    let(:key) {'foo'}
+    let(:value) {'$who bar'}
+    let(:channel) {'#testchan'}
+
+    before(:each) do
+      set_db_key_value(@db, key, value)
+    end
+
+    it 'should teach it' do
+      replies = get_replies_to("#{bot_nick}: #{key}")
+      expect(replies.length).to eq 1
+      expect(replies.first.text)
+        .to eq("#{key} is #{value.gsub('$who','test')}.")
+    end
+
+    it 'should literally teach it' do
+      replies = get_replies_to("#{bot_nick}: literal #{key}")
+      expect(replies.length).to eq 1
+      expect(replies.first.text)
+        .to eq("#{key} =is= #{value}.")
+    end
+
+    it 'should be able to forget it' do
+      replies = get_replies_to("#{bot_nick}: forget #{key}")
+      expect(replies.length).to eq 1
+      expect(replies.first.text).to eq("I forgot #{key}.")
+      replies = get_replies_to("#{bot_nick}: #{key}")
+      expect(replies.length).to eq 1
+      expect(replies.first).to give_up
+    end
+
+    it 'should be able to edit it' do
+      replies = get_replies_to(
+        "#{bot_nick}: #{key} =~ s/#{Regexp.escape(value)}/baz/")
+      expect(replies.length).to eq 1
+      expect(replies.first).to be_a_successful_edit
+      replies = get_replies_to("#{bot_nick}: #{key}")
+      expect(replies.length).to eq 1
+      expect(replies.first.text).to eq("#{key} is baz.")
+    end
+
+    it 'should be able to add to it' do
+      replies = get_replies_to("#{bot_nick}: #{key} is baz")
+      expect(replies.length).to eq 1
+      expect(replies.first).to be_an_acknowledgement
+      replies = get_replies_to("#{bot_nick}: #{key}")
+      expect(replies.length).to eq 1
+      expect(replies.first.text)
+        .to eq("#{key} is #{value.gsub('$who', 'test')} or baz.")
+    end
+  end
+
+  # TODO: add some tests around | special case
 end
